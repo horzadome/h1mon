bb985a51e8b08f0ad4f8bed779ddd0b67407f418
Davor Grubisa <horzadome@gmail.com>
2013-03-28 00:51:26 +0100

    Removed installation instructions from ingress.pl

--- ingress.pl
+++ ingress.pl
@@ -18,13 +18,6 @@ my $content_type = $parsed->content_type;
 
 
 # START Pushover API
-# To send notifications via Pushover:
-# 1) login to your Pushover account
-# 2) go to https://pushover.net/apps and create a new application
-# 3) Fill out the following lines with your Pushover APP TOKEN and your Pushover USER KEY
-# 4) Add this line to /etc/aliases: "h1mon:          | /opt/h1mon/ingress.pl"
-#    - make sure that this path is correct
-# 5) Configure your application (firewall or malware scanner) to send notifications to email "h1mon@localhost"
 LWP::UserAgent->new()->post(
   "https://api.pushover.net/1/messages.json", [
   "token" => "YOUR PUSHOVER APP TOKEN",
